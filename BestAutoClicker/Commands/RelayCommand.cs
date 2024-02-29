bae8368bf1da2f977a5751880c09e68789851c26
Sound <shehadyamer@gmail.com>
2024-02-29 17:00:39 +0200

    commands

---
+++ BestAutoClicker/Commands/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BestAutoClicker.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<Task> execute;
+        private readonly Func<bool> canExecute;
+        public RelayCommand(Func<Task> execute) : this(execute, canExecute: null)
+        {
+        }
+
+
+        public RelayCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute is null");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute();
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (execute != null)
+            {
+                await execute();
+            }
+        }
+    }
+}

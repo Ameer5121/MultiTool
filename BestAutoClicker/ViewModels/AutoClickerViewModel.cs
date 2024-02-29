485a247ed9a1186ceae6f47be11fb855520244cf
Sound <shehadyamer@gmail.com>
2024-02-29 02:24:14 +0200

    first

---
+++ BestAutoClicker/ViewModels/AutoClickerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestAutoClicker.ViewModels
+{
+    
+    internal class AutoClickerViewModel : ViewModelBase
+    {
+
+
+     
+        
+    }
+}

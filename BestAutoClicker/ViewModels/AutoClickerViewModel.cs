7ed3df9740f5f42b150f3dd8197e691dbc68a567
jsmeef <zeethasolr@gmail.com>
2024-03-01 19:51:56 +0100

    Sup

--- BestAutoClicker/ViewModels/AutoClickerViewModel.cs
+++ BestAutoClicker/ViewModels/AutoClickerViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BestAutoClicker.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BestAutoClicker.ViewModels
 {
@@ -10,8 +13,28 @@ namespace BestAutoClicker.ViewModels
     internal class AutoClickerViewModel : ViewModelBase
     {
 
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out Point x);
 
-     
+        public RelayCommand testing 
+        {
+
+            get
+            {
+                return new RelayCommand(ClickingFunction);
+            }
+        
+        }
+        private async Task ClickingFunction()
+        {
+
+            Point CurrentCursor;
+
+            GetCursorPos(out CurrentCursor);
+
+            MessageBox.Show(CurrentCursor.X.ToString() + ", " + CurrentCursor.Y.ToString());
+
+        }
         
     }
 }

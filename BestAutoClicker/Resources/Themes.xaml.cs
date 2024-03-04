1bccea9e6dd3309f3f5119fddde2f582c229f6c5
Sound <shehadyamer@gmail.com>
2024-03-04 23:59:14 +0200

    added custom titlebar

---
+++ BestAutoClicker/Resources/Themes.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BestAutoClicker.Resources
+{
+    public partial class Themes : ResourceDictionary
+    {
+        private void ShutDown(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void Minimize(object sender, RoutedEventArgs e) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+    }
+}

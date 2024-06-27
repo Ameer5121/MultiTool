5e228d4a7b49def64b70cb86f394176b3d7bb888
Sound <shehadyamer@gmail.com>
2024-06-28 02:57:04 +0300

    keyboarddatainput

--- BestAutoClicker/Helper/Structs/KeyboardMouseInput.cs
+++ BestAutoClicker/Helper/Structs/KeyboardMouseInput.cs
@@ -10,13 +10,14 @@ namespace BestAutoClicker.Helper.Structs
     {
         public int type;
         public MouseInputData mouseData;
-
+        public KeyboardInputData keyboardData;
 
         public KeyboardMouseInput(int type = 0)
         {
             this.type = type;
             mouseData = new MouseInputData();
             mouseData.dwFlags = 0x8001;
+            keyboardData = new KeyboardInputData();
         }
     }
 }
